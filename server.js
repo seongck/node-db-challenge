2ebeda626ff89b6889df66f12ed1c252f1dbc099
seongck <seongk@pm.me>
2019-11-22 20:23:54 -0800

    Add error handler middleware

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ server.use(helmet());
 server.use(express.json());
 server.use('/api/projects', ProjectRouter);
 
+function errorHandler(err, req, res, next) {
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error'
+  });
+}
+
+server.use(errorHandler);
+
 module.exports = server;
